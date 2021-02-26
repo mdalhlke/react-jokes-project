beee7d10098bcf2f7ec076311a92c17f6222402d
Maya Dahlke <mayadahlke@yahoo.com>
2021-02-25 19:26:05 -0800

    add unstyled searchbar

--- jokesapp/src/App.js
+++ jokesapp/src/App.js
@@ -1,48 +1,49 @@
-import React, {useEffect, useState} from 'react';
-import {Container, Row, Col, Spinner} from "react-bootstrap";
-import JokeList from "./Components/JokeList";
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
+import JokeList from './Components/JokeList';
 
 import './App.css';
+import SearchBar from './Components/SearchBar';
 
 function App() {
-  const [jokes, setJokes] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [jokes, setJokes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
+      .then((response) => response.json())
       .then((json) => {
-        setJokes(json)
-        setLoading(false)
-      })
-  }, [setLoading, setJokes])
+        setJokes(json);
+        setLoading(false);
+      });
+  }, [setLoading, setJokes]);
 
-
-  if(loading) {
-    return <Spinner animation="border" />
-}
+  if (loading) {
+    return <Spinner animation='border' />;
+  }
 
   return (
     <Container>
-      <Row 
-        className="justify-content-md-center"
-        style = {{marginTop:60}}>
-        <Col xs lg="2">
-          <h1 style = {{textAlign: "center"}}>Joke Generator</h1>
+      <Row className='justify-content-md-center' style={{ marginTop: 60 }}>
+        <Col xs lg='2'>
+          <h1 style={{ textAlign: 'center' }}>Joke Generator</h1>
         </Col>
       </Row>
 
-      <Row 
-        className="justify-content-md-center"
-        style = {{marginTop:20}}>
-        <Col xs lg="2">
-          <JokeList
-            jokes = {jokes}
-          />
+      <Row className='justify-content-md-center' style={{ marginTop: 20 }}>
+        <Col xs lg='2'>
+          <SearchBar />
+        </Col>
+      </Row>
+
+      <Row className='justify-content-md-center' style={{ marginTop: 20 }}>
+        <Col xs lg='2'>
+          <JokeList jokes={jokes} />
         </Col>
       </Row>
     </Container>
-  )}
+  );
+}
 
 export default App;
