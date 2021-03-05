79a36da7484ac14ee084b497cc86735df6a0ebb8
jf2024 <>
2021-03-04 17:49:03 -0800

    Added Names of Joke Genres

--- jokesapp/src/Components/Filter.jsx
+++ jokesapp/src/Components/Filter.jsx
@@ -5,8 +5,18 @@ import Checkbox from "@material-ui/core/Checkbox";
 
 export default function CheckboxLabels() {
   const [state, setState] = React.useState({
-    checkedA: true, //change "checkedA" and "checkedB" to names of what the filter is (like "Dark" or "Spooky")
-    checkedB: true,
+    checkProgramming: true,
+    checkMisc: true,
+    checkDark: true,
+    checkPun: true,
+    checkSpooky: true,
+    checkChrist: true,
+    checkNSFW: true,
+    checkReligious: true,
+    checkPolitical: true,
+    checkRacist: true,
+    checkSexist: true,
+    checkExplicit: true,
   });
 
   const handleChange = (event) => {
@@ -18,23 +28,123 @@ export default function CheckboxLabels() {
       <FormControlLabel
         control={
           <Checkbox
-            checked={state.checkedA}
+            checked={state.checkProgramming}
             onChange={handleChange}
-            name="checkedA"
+            name="checkProgramming"
           />
         }
-        label="Filter 1" //this is the name of the box, change it to appropriate filter name (like "dark" or "spooky")
+        label="Programming"
       />
       <FormControlLabel
         control={
           <Checkbox
-            checked={state.checkedB}
+            checked={state.checkMisc}
             onChange={handleChange}
-            name="checkedB"
+            name="checkMisc"
           />
         }
-        label="Filter 2" //this is the name of the box, change it to appropriate filter name (like "dark" or "spooky")
+        label="Miscellaneous"
       />
-    </FormGroup> //add more checkboxes inside the FormGroup later
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkDark}
+            onChange={handleChange}
+            name="checkDark"
+          />
+        }
+        label="Dark"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkPun}
+            onChange={handleChange}
+            name="checkPun"
+          />
+        }
+        label="Pun"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkSpooky}
+            onChange={handleChange}
+            name="checkSpooky"
+          />
+        }
+        label="Spooky"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkChrist}
+            onChange={handleChange}
+            name="checkChrist"
+          />
+        }
+        label="Christmas"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkNSFW}
+            onChange={handleChange}
+            name="checkNSFW"
+          />
+        }
+        label="NSFW"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkReligious}
+            onChange={handleChange}
+            name="checkReligious"
+          />
+        }
+        label="Religious"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkPolitical}
+            onChange={handleChange}
+            name="checkPolitical"
+          />
+        }
+        label="Political"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkRacist}
+            onChange={handleChange}
+            name="checkRacist"
+          />
+        }
+        label="Racist"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkSexist}
+            onChange={handleChange}
+            name="checkSexist"
+          />
+        }
+        label="Sexist"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkExplicit}
+            onChange={handleChange}
+            name="checkExplicit"
+          />
+        }
+        label="Explicit"
+      />
+    </FormGroup>
   );
 }
