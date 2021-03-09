59235835acaa0596b2ef5413a836dcd6a8545634
jf2024 <>
2021-03-08 17:30:59 -0800

    Put Blacklisted Filters in their own file

---
+++ jokesapp/src/Components/Blacklisted.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+
+export default function CheckboxLabels() {
+  const [state, setState] = React.useState({
+    checkNSFW: true,
+    checkReligious: true,
+    checkPolitical: true,
+    checkRacist: true,
+    checkSexist: true,
+    checkExplicit: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <FormGroup row>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkNSFW}
+            onChange={handleChange}
+            name="checkNSFW"
+          />
+        }
+        label="NSFW"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkReligious}
+            onChange={handleChange}
+            name="checkReligious"
+          />
+        }
+        label="Religious"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkPolitical}
+            onChange={handleChange}
+            name="checkPolitical"
+          />
+        }
+        label="Political"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkRacist}
+            onChange={handleChange}
+            name="checkRacist"
+          />
+        }
+        label="Racist"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkSexist}
+            onChange={handleChange}
+            name="checkSexist"
+          />
+        }
+        label="Sexist"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkExplicit}
+            onChange={handleChange}
+            name="checkExplicit"
+          />
+        }
+        label="Explicit"
+      />
+    </FormGroup>
+  );
+}
