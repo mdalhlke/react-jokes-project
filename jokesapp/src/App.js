edc51d475bcf9f391d2609d724b50a5134ca0a17
jf2024 <>
2021-03-08 17:29:09 -0800

    Updated Filters

--- jokesapp/src/App.js
+++ jokesapp/src/App.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Row, Col, Spinner } from 'react-bootstrap';
-import JokeList from './Components/JokeList';
-import SearchBar from './Components/SearchBar';
-import Filter from './Components/Filter';
-import GenerateButton from './Components/GenerateButton';
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Spinner } from "react-bootstrap";
+import JokeList from "./Components/JokeList";
+import SearchBar from "./Components/SearchBar";
+import Filter from "./Components/Filter";
+import Blacklisted from "./Components/Blacklisted";
+import GenerateButton from "./Components/GenerateButton";
 
 function App() {
   const [jokes, setJokes] = useState([]);
@@ -11,7 +12,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch('https://v2.jokeapi.dev/joke/Any?amount=10&safe-mode')
+    fetch("https://v2.jokeapi.dev/joke/Any?amount=10&safe-mode")
       .then((response) => response.json())
       .then((json) => {
         setJokes(json.jokes);
@@ -20,39 +21,41 @@ function App() {
   }, [setLoading, setJokes]);
 
   if (loading) {
-    return <Spinner animation='border' />;
+    return <Spinner animation="border" />;
   }
 
   return (
     <Container>
-      <Row className='justify-content-md-center' style={{ marginTop: 60 }}>
-        <Col xs lg='10'>
-          <h1 style={{ textAlign: 'center' }}>Joke Generator</h1>
+      <Row className="justify-content-md-center" style={{ marginTop: 60 }}>
+        <Col xs lg="10">
+          <h1 style={{ textAlign: "center" }}>Joke Generator</h1>
         </Col>
       </Row>
 
-      <Row className='justify-content-md-center' style={{ marginTop: 20 }}>
-        <Col xs lg='10'>
+      <Row className="justify-content-md-center" style={{ marginTop: 20 }}>
+        <Col xs lg="10">
           <GenerateButton />
         </Col>
       </Row>
 
-      <Row className='justify-content-md-center' style={{ marginTop: 20 }}>
-        <Col xs lg='10'>
+      <Row className="justify-content-md-center" style={{ marginTop: 20 }}>
+        <Col xs lg="10">
           <SearchBar />
         </Col>
       </Row>
 
-      <Row className='justify-content-md-center' style={{ marginTop: 200 }}>
-        <Col xs lg='20'>
+      <Row className="justify-content-md-center" style={{ marginTop: 200 }}>
+        <Col xs lg="20">
           <JokeList jokes={jokes} />
         </Col>
       </Row>
 
-      <Row className='justify-content-md-center' style={{ marginTop: -900 }}>
-        <Col xs lg='20'>
-          <h4 style={{ textAlign: 'left' }}>Filters</h4>
+      <Row classname="justify-content-md-center" style={{ marginTop: -925 }}>
+        <Col xs lg="20">
+          <h4 style={{ textAlign: "left" }}>Category Types</h4>
           <Filter />
+          <h4 style={{ textAlign: "left" }}>Blacklisted Types</h4>
+          <Blacklisted />
         </Col>
       </Row>
     </Container>
