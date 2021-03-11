787c609ede30877009ed3b69b1832aa71c10cd6b
Maya Dahlke <mayadahlke@yahoo.com>
2021-03-10 20:01:19 -0800

    get search string to pass to API

--- jokesapp/src/App.js
+++ jokesapp/src/App.js
@@ -1,21 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Spinner } from "react-bootstrap";
-import JokeList from "./Components/JokeList";
-import SearchBar from "./Components/SearchBar";
-import Filter from "./Components/Filter";
-import Blacklisted from "./Components/Blacklisted";
-import GenerateButton from "./Components/GenerateButton";
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
+import JokeList from './Components/JokeList';
+import SearchBar from './Components/SearchBar';
+import Filter from './Components/Filter';
+import Blacklisted from './Components/Blacklisted';
+import GenerateButton from './Components/GenerateButton';
 
-function App() {
+export default function App() {
   const [jokes, setJokes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState('');
 
-  const filters = ["Programming", "Miscellaneous", "Dark", "Pun", "Spooky", "Christmas"]
-  const blacklists = ["nsfw","religious","political","racist","sexist","explicit"]
+  const filters = [
+    'Programming',
+    'Miscellaneous',
+    'Dark',
+    'Pun',
+    'Spooky',
+    'Christmas',
+  ];
+  const blacklists = [
+    'nsfw',
+    'religious',
+    'political',
+    'racist',
+    'sexist',
+    'explicit',
+  ];
 
   useEffect(() => {
     setLoading(true);
-    fetch("https://v2.jokeapi.dev/joke/Any?amount=10&safe-mode")
+    fetch('https://v2.jokeapi.dev/joke/Any?amount=10&safe-mode')
       .then((response) => response.json())
       .then((json) => {
         setJokes(json.jokes);
@@ -23,18 +38,33 @@ function App() {
       });
   }, [setLoading, setJokes]);
 
+  function updateSearch(searchString) {
+    setSearch(searchString);
+  }
+
+  function handleClick() {
+    //TODO: FILL IN activeFilters AND activeBlacklist
+    //getJokes(search);
+    console.log(search);
+    console.log('clicked');
+  }
+
   function getJokes(activeFilters, activeBlacklist, search) {
     setLoading(true);
-    
+
     let filterInput = [];
     let blacklistInput = [];
-    
+
     for (let i = 0; i < activeFilters.length; i++) {
-      activeFilters[i] ? filterInput.push(filters[i]) : i = i
-      activeBlacklist[i] ? blacklistInput.push(blacklists[i]) : i = i
+      activeFilters[i] ? filterInput.push(filters[i]) : (i = i);
+      activeBlacklist[i] ? blacklistInput.push(blacklists[i]) : (i = i);
     }
 
-    fetch(`https://v2.jokeapi.dev/joke/${filterInput.join(",")}?blacklistFlags=${blacklistInput.join(",")}&contains=${search}amount=10`)
+    fetch(
+      `https://v2.jokeapi.dev/joke/${filterInput.join(
+        ','
+      )}?blacklistFlags=${blacklistInput.join(',')}&contains=${search}amount=10`
+    )
       .then((response) => response.json())
       .then((json) => {
         setJokes(json.jokes);
@@ -43,45 +73,45 @@ function App() {
   }
 
   if (loading) {
-    return <Spinner animation="border" style={{margin:"50%"}}/>;
+    return <Spinner animation='border' style={{ margin: '50%' }} />;
   }
 
   return (
     <Container>
-      <Row className="justify-content-md-center" style={{ marginTop: 60 }}>
-        <Col xs lg="10">
-          <h1 style={{ textAlign: "center" }}>Joke Generator</h1>
+      <Row className='justify-content-md-center' style={{ marginTop: 60 }}>
+        <Col xs lg='10'>
+          <h1 style={{ textAlign: 'center' }}>Joke Generator</h1>
         </Col>
       </Row>
 
-      <Row className="justify-content-md-center" style={{ marginTop: 20 }}>
-        <Col xs lg="10">
-          <SearchBar />
+      <Row className='justify-content-md-center' style={{ marginTop: 20 }}>
+        <Col xs lg='10'>
+          <SearchBar updateSearch={updateSearch} />
         </Col>
       </Row>
 
-      <Row classname="justify-content-md-center">
-        <Col xs lg="20">
-          <h4 style={{ textAlign: "left" }}>Category Types</h4>
+      <Row className='justify-content-md-center' style={{ marginTop: 20 }}>
+        <Col xs lg='20'>
+          <h4 style={{ textAlign: 'left' }}>Category Types</h4>
           <Filter />
-          <h4 style={{ textAlign: "left" }}>Blacklisted Types</h4>
+          <h4 style={{ textAlign: 'left' }}>Blacklisted Types</h4>
           <Blacklisted />
         </Col>
       </Row>
 
-      <Row className="justify-content-md-center">
-        <Col xs lg="10">
-          <GenerateButton />
+      <Row className='justify-content-md-center'>
+        <Col xs lg='10'>
+          <div onClick={handleClick}>
+            <GenerateButton />
+          </div>
         </Col>
       </Row>
 
-      <Row className="justify-content-md-center" style={{paddingTop: 20}}>
-        <Col xs lg="20">
+      <Row className='justify-content-md-center' style={{ paddingTop: 20 }}>
+        <Col xs lg='20'>
           <JokeList jokes={jokes} />
         </Col>
       </Row>
     </Container>
   );
 }
-
-export default App;
--- jokesapp/src/Components/SearchBar.jsx
+++ jokesapp/src/Components/SearchBar.jsx
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
-import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import React from 'react';
+import { InputGroup, FormControl } from 'react-bootstrap';
 
-export default function SearchBar() {
-  const [state, setState] = useState('');
-
-  const onSubmit = () => {
-    console.log(state);
-    //here figure out how to give this value to api
-  };
+export default function SearchBar(props) {
+  const { updateSearch } = props;
 
   return (
     <div>
@@ -17,23 +12,8 @@ export default function SearchBar() {
           aria-label='Search'
           aria-describedby='basic-addon2'
           type='text'
-          value={state}
-          onChange={(e) => setState(e.target.value)}
+          onChange={(e) => updateSearch(e.target.value)}
         />
-        <InputGroup.Append>
-          <Button variant='outline-success' onClick={onSubmit}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              className='bi bi-search'
-              viewBox='0 0 16 16'
-            >
-              <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
-            </svg>
-          </Button>
-        </InputGroup.Append>
       </InputGroup>
     </div>
   );
